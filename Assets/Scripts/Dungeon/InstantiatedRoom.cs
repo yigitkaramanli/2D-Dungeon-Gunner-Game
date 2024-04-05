2ca7d5ee37b223b36e9e03bb8c8652536e5b39bf
Yigit Karamanli <yigit.karamanli@hotmail.com>
2024-04-05 19:04:44 +0300

    Backup for changing pcs

--- Assets/Scripts/Dungeon/InstantiatedRoom.cs
+++ Assets/Scripts/Dungeon/InstantiatedRoom.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class InstantiatedRoom : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [HideInInspector]
+    public Room room;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [HideInInspector]
+    public Grid grid;
+
+    [HideInInspector]
+    public Tilemap groundTilemap;
+
+    [HideInInspector]
+    public Tilemap decoration1Tilemap;
+
+    [HideInInspector]
+    public Tilemap decoration2Tilemap;
+
+    [HideInInspector]
+    public Tilemap frontTilemap;
+
+    [HideInInspector]
+    public Tilemap collisionTilemap;
+
+    [HideInInspector]
+    public Tilemap minimapTilemap;
+
+    [HideInInspector]
+    public Bounds roomColliderBounds;
 }
