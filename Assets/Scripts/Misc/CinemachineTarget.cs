d1108c009a42ca84be9f19ef1c3333d64bf1f156
Yiğit Karamanlı <yigitkaramanli@sabanciuniv.edu>
2024-04-21 16:46:57 +0300

    Updated the CinemachineTarget.cs so that the cinemachine camera follows both the player and the cursor.

--- Assets/Scripts/Misc/CinemachineTarget.cs
+++ Assets/Scripts/Misc/CinemachineTarget.cs
@@ -9,6 +9,12 @@ public class CinemachineTarget : MonoBehaviour
 {
     private CinemachineTargetGroup cinemachineTargetGroup;
 
+    #region Tooltip
+    [Tooltip("Populate with the CursorTarget gameobject.")]
+    #endregion
+    [SerializeField]
+    private Transform cursorTarget;
+
     private void Awake()
     {
         cinemachineTargetGroup = GetComponent<CinemachineTargetGroup>();
@@ -21,12 +27,21 @@ public class CinemachineTarget : MonoBehaviour
 
     private void SetCinemachineTargetGroup()
     {
-        CinemachineTargetGroup.Target cinemachineTarget_player = new CinemachineTargetGroup.Target
-            { weight = 1f, radius = 1f, target = GameManager.Instance.GetPlayer().transform };
+        //Create the target group for the cinemachine camera to follow - group will include the player and screen cursor
+        CinemachineTargetGroup.Target cinemachineGroupTarget_player = new CinemachineTargetGroup.Target
+            { weight = 1f, radius = 2.5f, target = GameManager.Instance.GetPlayer().transform };
 
+        CinemachineTargetGroup.Target cinemachineGroupTarget_cursor = new CinemachineTargetGroup.Target
+            { weight = 1f, radius = 1f, target = cursorTarget };
+        
         CinemachineTargetGroup.Target[] cinemachineTargetArray = new CinemachineTargetGroup.Target[]
-            { cinemachineTarget_player };
+            { cinemachineGroupTarget_player, cinemachineGroupTarget_cursor };
 
         cinemachineTargetGroup.m_Targets = cinemachineTargetArray;
+    }
+
+    private void Update()
+    {
+        cursorTarget.position = HelperUtilities.GetMouseWorldPosition();
     }
 }
