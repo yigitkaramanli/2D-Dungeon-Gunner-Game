c237767062b45c28f0db7071e487acf6591f21ea
Yiğit Karamanlı <yigitkaramanli@sabanciuniv.edu>
2024-09-05 12:36:23 +0300

    Forgot to add some files to the last commit.

---
+++ Assets/Scripts/Minimap/Minimap.cs
@@ -0,0 +1,47 @@
+using System;
+using Cinemachine;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Minimap : MonoBehaviour
+{
+   #region ToolTip
+   [Tooltip("Populate with the child MinimapPlayer gameObject")]
+   #endregion
+
+   [SerializeField]
+   private GameObject miniMapPlayer;
+
+   private Transform playerTransform;
+
+   private void Start()
+   {
+      playerTransform = GameManager.Instance.GetPlayer().transform;
+
+      CinemachineVirtualCamera cinemachineVirtualCamera = GetComponentInChildren<CinemachineVirtualCamera>();
+      cinemachineVirtualCamera.Follow = playerTransform;
+
+      SpriteRenderer spriteRenderer = miniMapPlayer.GetComponent<SpriteRenderer>();
+      if (spriteRenderer != null)
+      {
+         spriteRenderer.sprite = GameManager.Instance.GetPlayerMinimapIcon();
+      }
+   }
+
+   private void Update()
+   {
+      if (playerTransform != null && miniMapPlayer != null)
+      {
+         miniMapPlayer.transform.position = playerTransform.position;
+      }
+   }
+
+   #region Validation
+#if UNITY_EDITOR
+   private void OnValidate()
+   {
+      HelperUtilities.ValidateCheckNullValue(this, nameof(miniMapPlayer), miniMapPlayer);
+   }
+#endif
+   #endregion
+}
